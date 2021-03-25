0df15a7c9481d7e57354fe5d1e7c21330d3912d3
Combatd <combatdstar@gmail.com>
2021-03-25 09:37:44 -0700

    Render DOM in App.tsx

--- src/App.js
+++ src/App.js
@@ -1,1 +1,5 @@
-var my_name = "Mark";
+"use strict";
+exports.__esModule = true;
+var jsx_runtime_1 = require("react/jsx-runtime");
+var ReactDOM = require("react-dom");
+ReactDOM.render(jsx_runtime_1.jsx("h1", { children: "This is JSX" }, void 0), document.querySelector('#root'));
