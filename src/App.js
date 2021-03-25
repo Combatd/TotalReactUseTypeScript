d4c44ef07ffc90a51e9bb3f1765a4ff4e63a8a48
Combatd <combatdstar@gmail.com>
2021-03-25 14:00:20 -0700

    Render FruitLoops on Parent App

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 var Header_1 = require("./Header");
 var CustomDate_1 = require("./CustomDate");
+var FruitLoops_1 = require("./FruitLoops");
 ReactDOM.render(React.createElement("div", null,
     React.createElement(Header_1.default, { text: "Satisfied as text type" }),
     React.createElement(CustomDate_1.CustomDate, null),
+    React.createElement(FruitLoops_1.FruitLoops, null),
     React.createElement("p", null, "this is a paragraph!!"),
     React.createElement("a", { href: "#" }, "Click me")), document.querySelector('#root'));
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Header from './Header';
 import { CustomDate } from './CustomDate';
+import { FruitLoops } from './FruitLoops';
 
 ReactDOM.render(
     <div>
         <Header text="Satisfied as text type"/>
         <CustomDate />
+        <FruitLoops />
         <p>this is a paragraph!!</p>
         <a href="#">Click me</a>
     </div>,
