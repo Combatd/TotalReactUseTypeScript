ad0741c43f5173eb5a2a6ff2f479bc7ba5dea147
Combatd <combatdstar@gmail.com>
2021-03-25 13:52:11 -0700

    Handle click on button for date and time

--- src/CustomDate/index.js
+++ src/CustomDate/index.js
@@ -4,11 +4,22 @@ exports.CustomDate = void 0;
 var React = require("react");
 function CustomDate() {
     var _a = React.useState(""), date = _a[0], setDate = _a[1];
+    var _b = React.useState(""), time = _b[0], setTime = _b[1];
     var getDate = function () {
         setDate(new Date().toDateString());
     };
+    var getTime = function () {
+        setTime(new Date().toTimeString());
+    };
+    var handleGetTimeDate = function (e) {
+        getDate();
+        getTime();
+    };
     return (React.createElement("div", null,
         date,
-        React.createElement("button", { onClick: getDate }, "Get the Date")));
+        React.createElement("br", null),
+        time,
+        React.createElement("br", null),
+        React.createElement("button", { onClick: handleGetTimeDate }, "Get the Date and time")));
 }
 exports.CustomDate = CustomDate;
--- src/CustomDate/index.tsx
+++ src/CustomDate/index.tsx
@@ -2,15 +2,27 @@ import * as React from 'react';
 
 function CustomDate() {
     let [date, setDate] = React.useState(""); 
+    let [time, setTime] = React.useState("");
 
     let getDate = () => {
         setDate(new Date().toDateString());
     }
+    let getTime = function() {
+        setTime(new Date().toTimeString());
+    }
+
+    let handleGetTimeDate = (e) => {
+        getDate();
+        getTime();
+    } 
 
     return (
         <div>
             {date}
-            <button onClick={ getDate }>Get the Date</button>
+            <br/>
+            {time}
+            <br/>
+            <button onClick={ handleGetTimeDate }>Get the Date and time</button>
         </div>
     )
 }
